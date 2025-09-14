7e3ff71a664a5b719be38193d687ce7c458e1f01
Quentin Biache <nitrogenium@outlook.fr>
2025-09-14 13:57:49 +0200

    completed doc

--- src/npn.m
+++ src/npn.m
@@ -8,14 +8,38 @@
 % -----------------------------------------------------------------------------
 % Best viewed with space indentation (2 spaces)
 % =============================================================================
+
+% =============================================================================
+% DESCRIPTION
+% =============================================================================
+% Piecewise linear model for the BJT collector current vs base-emitter voltage.
 %
+% Model is not meant to be a super accurate model of a BJT but rather a simple
+% non-trivial emulation that aims at exploring the role of feedback effect in 
+% amplifiers etc.
 %
-% Parameters:
+% Model has 2 regions:
+% - forward active    (     v_be >= 0.7V)
+% - cutoff            (0 =< v_be <  0.7V)
+%
+% Reverse models, saturation, breakdown modes etc. are not implemented since 
+% it's beyond the purpose of this crude model.
+% Any situation outside the scope above will return i_c = 0.
+
+
+
+% =============================================================================
+% PARAMETERS
+% =============================================================================
 % - param.v_be_th   : threshold 'v_be' voltage (in V). Usually ~0.7V
 % - param.i_th      : collector current when v_be = v_be_th (in A). Usually a few mA
 % - param.g_m       : bjt transconductance (in A/V)
 
 
+
+% =============================================================================
+% MODEL
+% =============================================================================
 function i_c = npn(v_be, param)
 
   % Initialise output
--- src/tb_fp_npn.m
+++ src/tb_fp_npn.m
@@ -17,16 +17,27 @@
 
 
 
+% =============================================================================
+% SETTINGS
+% =============================================================================
+
+% Number of test cases
+nPts = 100;
+
+% BJT settings
 bjtParam.v_be_th = 0.7;
 bjtParam.i_th = 5e-3;
 bjtParam.g_m = 100;
 
 
-% Draw random values for 'a' and 'b', makes sure 
-% that both solutions satisfy I = npn(a+bI)
 
+% =============================================================================
+% MAIN
+% =============================================================================
+% Draw random values for 'a' and 'b', make sure that both solutions returned by 
+% 'fp_npn' satisfy I = npn(a+bI)
+% - a = -10 ... 10 Volts
+% - b = 1/r with r = 0.1 ... 100 ohms
 
 [i_fp_active, i_fp_cutoff] = fp_npn(a, b, param)
 
-% - a = -10 ... 10 Volts
-% - b = 1/r with r = 0.1 ... 100 ohms
