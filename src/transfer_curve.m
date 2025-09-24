a52bbb12dad47d6f1ce92adfec0504cab7482567
Quentin Biache <nitrogenium@outlook.fr>
2025-09-24 22:39:12 +0200

    added project for the board design

--- src/transfer_curve.m
+++ src/transfer_curve.m
@@ -21,9 +21,8 @@ clear all
 clc
 
 
-% =============================================================================
-% SETTINGS
-% =============================================================================
+
+%% SETTINGS
 
 % BJT settings
 bjtParam.v_be_th = 0.7;
@@ -33,16 +32,13 @@ bjtParam.g_m = 100;
 % Simulation points
 nPts = 500;
 
-V_bias = 1.6;
-delta_V = linspace(-0.5, 0.5, nPts)';
+V_bias = 1.2;
 R_e = 4;
 R_load = 32;
 
-% =============================================================================
-% MAIN
-% =============================================================================
 
 
+%% SETUP 1
 
 % From KVL:
 % I_s = npn(V_bias/2 - delta_V - R_e*I_s)
@@ -50,20 +46,51 @@ R_load = 32;
 % With:
 % - a = V_bias/2 - delta_V
 % - b = -R_e
-
+delta_V = linspace(-0.5, 0.5, nPts)';
 [I_s_active, I_s_cutoff, ~] = fp_npn(V_bias/2 - delta_V, -R_e, bjtParam);
 [I_d_active, I_d_cutoff, ~] = fp_npn(V_bias/2 + delta_V, -R_e, bjtParam);
 
+figure
 plot(...
   delta_V, I_s_active, 'b', ...
   delta_V, I_s_cutoff, 'b-.', ...
   delta_V, -I_d_active, 'r', ...
-  delta_V, -I_d_cutoff, 'r-.' ...
+  delta_V, -I_d_cutoff, 'r-.', ...
+  delta_V, R_load*(I_s_active-I_d_active), 'k', ...
+  delta_V, R_load*(I_s_cutoff-I_d_active), 'k', ...
+  delta_V, R_load*(I_s_active-I_d_cutoff), 'k' ...
 )
 grid on
 xlabel('\DeltaV = V_O - V_I')
 legend('I_S (active)', 'I_S (cutoff)', 'I_D (active)', 'I_D (cutoff)')
 
+
+
+%% SETUP 2
+
+% figure
+% for V_I = -1.5:0.05:1.5
+%   V_O = linspace(-1.5, 1.5, nPts)';
+%   [I_s_active, I_s_cutoff, ~] = fp_npn(V_bias/2 - (V_O-V_I), -R_e, bjtParam);
+%   [I_d_active, I_d_cutoff, ~] = fp_npn(V_bias/2 + (V_O-V_I), -R_e, bjtParam);
+% 
+%   V_O_th = R_load*(I_s_active-I_d_active);
+% 
+%   plot(...
+%     V_O, V_O, 'b', ...
+%     V_O, R_load*(I_s_active-I_d_active), 'r', ...
+%     V_O, R_load*(I_s_cutoff-I_d_active), 'r-.', ...
+%     V_O, R_load*(I_s_active-I_d_cutoff), 'r-.' ...
+%   )
+%   grid on
+%   xlim([-1.5 1.5])
+%   ylim([-1.5 1.5])
+%   pause(0.05)
+% end
+% %xlabel('\DeltaV = V_O - V_I')
+% %legend('I_S (active)', 'I_S (cutoff)', 'I_D (active)', 'I_D (cutoff)')
+% 
+% 
 
 
 
